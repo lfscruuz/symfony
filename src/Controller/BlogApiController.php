d6e0f36d4a9863acc757b2b1388a036151fdcccd
Luis Felipe Cruz <lfscruz@outlook.com>
2024-02-15 12:45:59 -0300

    lista de posts

---
+++ src/Controller/BlogApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class BlogApiController extends AbstractController{
+    // public function show(int $id) : Response{
+
+    // }
+}
---
+++ src/Controller/BlogController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController {
+    #[Route('/blog', name:'blog_list')]
+    public function list() : Response{
+        return new Response("<html><body>AQUI</body></html>");
+    }
+
+    #[Route("/blog/{user}", name:"blog_user")]
+    public function show(string $user): Response{
+        $posts = [
+            'post 1',
+            'post 2',
+            'post 3',
+            'post 4',
+            'post 5',
+            'post 6',
+        ];
+        $user = ucwords(str_replace("-"," ", $user));
+        return $this->render('blog/lista.twig', [
+            'user'=> $user,
+            'posts' => $posts
+        ]);
+    }
+}
